8e0d040c22e4f8bf2336d14fde82e14884e2da03
Wrose-05 <165732709+Wrose-05@users.noreply.github.com>
2025-06-09 10:18:40 -0300

    Update api.js

--- aula 1/Gabarito aula 1/api/api.js
+++ aula 1/Gabarito aula 1/api/api.js
@@ -4,7 +4,6 @@ import axios from "axios";
 const URL = "http://localhost:3000"
 
 //RODAR COM DOCKER
-// Use o nome do serviço definido no docker-compose.yml
 //const URL = "http://express_backend:3001";
 
 
